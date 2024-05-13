65e75d5b58f01a0b8c5955de3918f659e45a5d8b
Vimal Kumar Meena <99386630+Vimal-Meena@users.noreply.github.com>
2024-05-13 13:01:01 +0530

    Day 260 LeetCode Daily Problem

---
+++ 2024-05-13(Day 260) [861. Score After Flipping Matrix].java
@@ -0,0 +1,58 @@
+// #LeetCode:861
+
+class Solution {
+    public static int matrixScore(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        for(int i=0;i<m;i++){
+            if(grid[i][0]==0){
+                for(int j=0;j<n;j++){
+                    if(grid[i][j]==0){
+                        grid[i][j]=1;
+                    } else{
+                        grid[i][j] = 0;
+                    }
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+        // if(grid[i][0]==0){
+            int count=0;
+                for(int j=0;j<m;j++){
+                    
+                    if(grid[j][i]==0){
+                        count++;
+                    }
+                }
+                if(count>grid.length/2){
+                    for(int j=0;j<m;j++){
+                        if(grid[j][i]==0){
+                            grid[j][i]=1;
+                        } else{
+                            grid[j][i] = 0;
+                        }
+                    }
+                }
+            
+        }
+       int ans = 0; 
+        for(int i=0;i<m;i++){
+            
+            int pow = n-1;
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    ans += Math.pow(2,pow);
+                }
+                pow--;
+                
+            }
+            
+        }
+
+        return ans;
+    }
+    public static void main(String args[]){
+        int grid[][] = {{0,0,1,1},{1,0,1,0},{1,1,0,0}};
+        System.out.println(matrixScore(grid));
+    }
+}
